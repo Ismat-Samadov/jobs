b01150637e5d8c1bdd7abab68c7099d200d74ab4
Ismat-Samadov <ismetsemedov@gmail.com>
2024-04-13 23:08:08 +0400

    check

--- database.py
+++ database.py
@@ -6,13 +6,13 @@ def create_session():
     DB_HOST = os.environ.get('DB_HOST')
     DB_PORT = os.environ.get('DB_PORT')
     DB_USER = os.environ.get('DB_USER')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_PSWD = os.environ.get('DB_PASSWORD')
     DB_NAME = os.environ.get('DB_NAME')
 
-    if None in [DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME]:
+    if None in [DB_HOST, DB_PORT, DB_USER, DB_PSWD, DB_NAME]:
         raise EnvironmentError("One or more required environment variables are missing.")
 
-    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PSWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     print(f"Database URL: {DATABASE_URL}")
 
     engine = create_engine(DATABASE_URL)
