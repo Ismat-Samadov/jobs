6879c5ca4fea3602d780286442a176aa3dcfe488
Ismat-Samadov <ismetsemedov@gmail.com>
2024-04-15 23:31:34 +0400

    check

--- app/api.py
+++ app/api.py
@@ -1,5 +1,5 @@
 # app/api.py
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Query
 from database import connect_to_postgres, close_connection
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
@@ -32,7 +32,7 @@ async def read_root():
     return {"message": "Welcome to your FastAPI application!"}
 @app.get("/")
 async def read_root():
-    return {"message": "Welcome to your FastAPI application!"}
+    return {"message": "Welcome to your JobAPI "}
 
 @app.get("/data/")
 async def get_data(db=Depends(connect_to_postgres)):
@@ -41,12 +41,6 @@ async def get_data(db=Depends(connect_to_postgres)):
     await close_connection(db)
     return rows
 
-@app.get("/data/id/{id}")
-async def get_data_by_id(id: int, db=Depends(connect_to_postgres)):
-    query = "SELECT * FROM vacancy_table WHERE id = $1;"
-    row = await db.fetchrow(query, id)
-    await close_connection(db)
-    return row
 
 @app.get("/data/company/{company}")
 async def get_data_by_company(company: str, db=Depends(connect_to_postgres)):
@@ -54,25 +48,16 @@ async def get_data_by_company(company: str, db=Depends(connect_to_postgres)):
     rows = await db.fetch(query, company)
     await close_connection(db)
     return rows
+@app.get("/data/position/")
+async def get_data_by_position(position: str = Query(..., description="Position name to search for"), db=Depends(connect_to_postgres)):
+    query = "SELECT * FROM vacancy_table WHERE position ILIKE $1;"
+    rows = await db.fetch(query, f"%{position}%")
+    await close_connection(db)
+    return rows
 @app.post("/data/")
 async def create_data(data: dict, db=Depends(connect_to_postgres)):
     query = "INSERT INTO vacancies (title, description) VALUES ($1, $2) RETURNING *;"
     values = (data.get("title"), data.get("description"))
     row = await db.fetchrow(query, *values)
-    await close_connection(db)
-    return row
-
-@app.put("/data/{id}")
-async def update_data(id: int, data: dict, db=Depends(connect_to_postgres)):
-    query = "UPDATE vacancy_table SET column1 = $1, column2 = $2 WHERE id = $3 RETURNING *;"
-    values = (data.get("column1"), data.get("column2"), id)
-    row = await db.fetchrow(query, *values)
-    await close_connection(db)
-    return row
-
-@app.delete("/data/{id}")
-async def delete_data(id: int, db=Depends(connect_to_postgres)):
-    query = "DELETE FROM vacancy_table WHERE id = $1 RETURNING *;"
-    row = await db.fetchrow(query, id)
     await close_connection(db)
     return row
