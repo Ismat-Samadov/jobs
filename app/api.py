bb21e907bc0d9566071b8df5a73711f2fe805c2c
Ismat-Samadov <ismetsemedov@gmail.com>
2024-04-15 23:14:30 +0400

    check

--- app/api.py
+++ app/api.py
@@ -8,7 +8,10 @@ app = FastAPI()
 # CORS settings to allow only the specified origin
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://jobapi.netlify.app/"],
+    allow_origins=[
+        "https://jobapi.netlify.app",
+        "https://ismat-samadov.github.io"
+    ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
