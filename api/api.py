bba9fd2b2f5e2696bf3e9c0d2a19d5ff6a92697c
Ismat-Samadov <ismetsemedov@gmail.com>
2024-04-16 19:05:46 +0400

    check

--- api/api.py
+++ api/api.py
@@ -32,27 +32,21 @@ async def read_root():
     return {"message": "Welcome to your JobAPI "}
 
 
-@app.get("/data/company/{company}")
-async def get_data_by_company(company: str, db=Depends(connect_to_postgres)):
-    query = "SELECT * FROM vacancy_table WHERE company = $1;"
-    rows = await db.fetch(query, company)
-    await close_connection(db)
-    return rows
-@app.get("/data/position/")
-async def get_data_by_position(position: str = Query(..., description="Position name to search for"), db=Depends(connect_to_postgres)):
-    query = "SELECT * FROM vacancy_table WHERE vacancy ILIKE $1;"
-    rows = await db.fetch(query, f"%{position}%")
-    await close_connection(db)
-    return rows
+# @app.get("/data/company/{company}")
+# async def get_data_by_company(company: str, db=Depends(connect_to_postgres)):
+#     query = "SELECT * FROM vacancy_table WHERE company = $1;"
+#     rows = await db.fetch(query, company)
+#     await close_connection(db)
+#     return rows
+# @app.get("/data/position/")
+# async def get_data_by_position(position: str = Query(..., description="Position name to search for"), db=Depends(connect_to_postgres)):
+#     query = "SELECT * FROM vacancy_table WHERE vacancy ILIKE $1;"
+#     rows = await db.fetch(query, f"%{position}%")
+#     await close_connection(db)
+#     return rows
 
 
-@app.post("/data/")
-async def create_data(data: dict, db=Depends(connect_to_postgres)):
-    query = "INSERT INTO vacancies (title, description) VALUES ($1, $2) RETURNING *;"
-    values = (data.get("title"), data.get("description"))
-    row = await db.fetchrow(query, *values)
-    await close_connection(db)
-    return row
+
 @app.get("/data/")
 async def get_data(
     company: str = Query(None, description="Company name to search for (partial match)"),
