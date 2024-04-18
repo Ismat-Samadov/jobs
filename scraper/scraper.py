2ee170167a4b7a0feb89d964599819e8bc53de1d
Ismat-Samadov <ismetsemedov@gmail.com>
2024-04-18 17:54:30 +0400

    check

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -139,18 +139,47 @@ class JobScraper:
         df = pd.DataFrame(job_vacancies)
         return df
     
+
+    def scrape_hellojob_az():
+        job_vacancies = []
+
+        for page_number in range(1, 11):  
+            url = f"https://www.hellojob.az/vakansiyalar?page={page_number}"
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                job_listings = soup.find_all('a', class_='vacancies__item')
+
+                for job in job_listings:
+                    company_name = job.find('p', class_='vacancy_item_company').text.strip()
+                    vacancy_title = job.find('h3').text.strip()
+                    apply_link = "https://www.hellojob.az" + job['href']
+
+                    job_vacancies.append({"company": company_name, "vacancy": vacancy_title, "apply_link": apply_link})
+
+            else:
+                print(f"Failed to retrieve page {page_number}. Status code:", response.status_code)
+
+        return pd.DataFrame(job_vacancies)
+    
     def get_data(self):
         abb_df = self.scrape_abb()
         azerconnect_df = self.scrape_azerconnect()
         pashabank_df = self.scrape_pashabank()
         azercell_df = self.scrape_azercell()
         busy_az_df = self.scrape_busy_az() 
+        hellojob_az_df = self.scrape_hellojob_az() 
+        
         scrape_date = datetime.now()
+
         abb_df['scrape_date'] = scrape_date
         azerconnect_df['scrape_date'] = scrape_date
         pashabank_df['scrape_date'] = scrape_date
         azercell_df['scrape_date'] = scrape_date
         busy_az_df['scrape_date'] = scrape_date  
+        hellojob_az_df['scrape_date'] = scrape_date
+        
         self.data = pd.concat([pashabank_df,
                                azerconnect_df,
                                azercell_df,
