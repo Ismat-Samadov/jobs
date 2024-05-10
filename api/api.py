216fe97119431de476ee2f26b18fd0eb2a8f7fda
Ismat-Samadov <ismetsemedov@gmail.com>
2024-05-10 11:55:19 +0400

    api.py modified commented code

--- api/api.py
+++ api/api.py
@@ -2,6 +2,8 @@
 from fastapi import FastAPI, Depends, Query
 from database import connect_to_postgres, close_connection
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Query
+
 app = FastAPI()
 
 # CORS settings to allow only the specified origin
@@ -41,32 +43,6 @@ async def get_data_by_position(position: str = Query(..., description="Position 
     await close_connection(db)
     return rows
 
-# @app.get("/data/")
-# async def get_data(
-#     company: str = Query(None, description="Company name to search for (partial match)"),
-#     position: str = Query(None, description="Position name to search for"),
-#     db=Depends(connect_to_postgres)
-# ):
-#     if company is None and position is None:
-#         # If neither company nor position is provided, return all data
-#         query = "SELECT * FROM vacancy_table ORDER BY scrape_date DESC;"
-#         rows = await db.fetch(query)
-#     elif company is not None and position is not None:
-#         # If both company and position are provided, search by both
-#         query = "SELECT * FROM vacancy_table WHERE company ILIKE $1 AND vacancy ILIKE $2 ORDER BY scrape_date DESC;"
-#         rows = await db.fetch(query, f"%{company}%", f"%{position}%")
-#     elif company is not None:
-#         # If only company is provided, search by company with LIKE
-#         query = "SELECT * FROM vacancy_table WHERE company ILIKE $1 ORDER BY scrape_date DESC;"
-#         rows = await db.fetch(query, f"%{company}%")
-#     else:
-#         # If only position is provided, search by position
-#         query = "SELECT * FROM vacancy_table WHERE vacancy ILIKE $1 ORDER BY scrape_date DESC;"
-#         rows = await db.fetch(query, f"%{position}%")
-#     await close_connection(db)
-#     return rows
-
-from fastapi import Query
 
 @app.get("/data/")
 async def get_data(
