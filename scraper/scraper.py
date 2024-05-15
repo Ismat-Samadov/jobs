c0b711521aa9769d51e49f37954d50eef44f486b
Ismat-Samadov <ismetsemedov@gmail.com>
2024-05-15 05:45:22 +0400

    added new scraper for boss_az

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -173,6 +173,30 @@ class JobScraper:
             return pd.DataFrame(job_vacancies)
         else:
             return pd.DataFrame(columns=['company', 'vacancy', 'apply_link'])
+    def scrape_boss_az(self):
+        print("Starting to scrape Boss.az...")
+        job_vacancies = []
+        for page_num in range(1, 21):  # Scrape from page 1 to 20
+            url = f"https://boss.az/vacancies?page={page_num}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                job_listings = soup.find_all('div', class_='results-i')
+                for job in job_listings:
+                    title = job.find('h3', class_='results-i-title').get_text(strip=True)
+                    company = job.find('a', class_='results-i-company').get_text(strip=True)
+                    link = f"https://boss.az{job.find('a', class_='results-i-link')['href']}"
+                    job_vacancies.append({
+                        "company": company,
+                        "vacancy": title,
+                        "apply_link": link
+                    })
+                print(f"Scraped {len(job_listings)} jobs from page {page_num}")
+            else:
+                print(f"Failed to retrieve page {page_num}. Status code: {response.status_code}")
+
+        return pd.DataFrame(job_vacancies)
+
 
     def get_data(self):
         abb_df = self.scrape_abb()
@@ -181,6 +205,9 @@ class JobScraper:
         azercell_df = self.scrape_azercell()
         busy_az_df = self.scrape_busy_az()
         hellojob_az_df = self.scrape_hellojob_az()
+        boss_az_df = self.scrape_boss_az()
+
+
 
 
         scrape_date = datetime.now()
@@ -191,6 +218,8 @@ class JobScraper:
         azercell_df['scrape_date'] = scrape_date
         busy_az_df['scrape_date'] = scrape_date
         hellojob_az_df['scrape_date'] = scrape_date
+        boss_az_df['scrape_date'] = scrape_date
+
 
 
         self.data = pd.concat([pashabank_df,
@@ -198,5 +227,6 @@ class JobScraper:
                                azercell_df,
                                abb_df,
                                busy_az_df,
-                               hellojob_az_df],
+                               hellojob_az_df,
+                               boss_az_df],
                               ignore_index=True)
