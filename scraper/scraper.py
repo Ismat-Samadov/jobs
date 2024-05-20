9e75d0a870b8225a9c9c6f1c86dd8db9417a0c5b
Ismat-Samadov <ismetsemedov@gmail.com>
2024-05-20 16:26:40 +0400

    added offer az scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -473,6 +473,65 @@ class JobScraper:
         print("Scraping completed for Xalqbank")
         return df
 
+    def offer_az(self):
+        start_page = 1
+        end_page = 10
+        try:
+            print("Started scraping offer.az")
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Encoding': 'gzip, deflate, br, zstd',
+                'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,ru;q=0.7,az;q=0.6',
+                'Cache-Control': 'max-age=0',
+                'Referer': 'https://www.google.com/',
+                'Dnt': '1',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-User': '?1',
+                'Upgrade-Insecure-Requests': '1'
+            }
+
+            vacancies = []
+
+            for page in range(start_page, end_page + 1):
+                url = f'https://www.offer.az/is-elanlari/page/{page}/'
+                response = requests.get(url, headers=headers)
+                response.raise_for_status()  # Ensure the request was successful
+
+                soup = BeautifulSoup(response.content, 'html.parser')
+
+                job_elements = soup.find_all('div', class_='job-card')
+
+                # Debugging: Check the number of job elements found
+                print(f"Found {len(job_elements)} job elements on page {page}")
+
+                for job_element in job_elements:
+                    title_tag = job_element.find('a', class_='job-card__title')
+                    company_tag = job_element.find('p', class_='job-card__meta')
+                    location_tag = job_element.find_all('p', class_='job-card__meta')[1]
+                    date_tag = job_element.find_all('p', class_='job-card__meta')[1]
+
+                    title = title_tag.text.strip() if title_tag else 'No title'
+                    company = company_tag.text.strip() if company_tag else 'No company'
+                    location = location_tag.text.strip().split(' - ')[1] if location_tag else 'No location'
+                    date = date_tag.text.strip().split(' - ')[0] if date_tag else 'No date'
+                    link = title_tag['href'] if title_tag and 'href' in title_tag.attrs else 'No link'
+
+                    vacancies.append([company, title, link])
+
+                    # Debugging: Print each job element
+                    print(f" Company: {company}, Vacancy: {title}, Link: {link}")
+
+            df = pd.DataFrame(vacancies, columns=['company','vacancy','apply_link'])
+            print("Scraping completed for offer.az")
+            return df
+        except Exception as e:
+            print(f"Offer.az scraping failed: {e}")
+            return pd.DataFrame(columns=['company', 'vacancy', 'apply_link'])
+
+
 
     def get_data(self):
         abb_df = self.abb()
@@ -490,6 +549,7 @@ class JobScraper:
         banker_az_df = self.banker_az()
         smartjob_az_df = self.smartjob_az()
         xalqbank_df = self.xalqbank()
+        offer_az_df = self.offer_az()
 
 
 
@@ -510,6 +570,8 @@ class JobScraper:
         banker_az_df['scrape_date'] = scrape_date
         smartjob_az_df['scrape_date'] = scrape_date
         xalqbank_df['scrape_date'] = scrape_date
+        offer_az_df['scrape_date'] = scrape_date
+
 
         self.data = pd.concat([pashabank_df,
                                azerconnect_df,
@@ -525,6 +587,7 @@ class JobScraper:
                                bank_respublika_df,
                                banker_az_df,
                                smartjob_az_df,
-                               xalqbank_df],
+                               xalqbank_df,
+                               offer_az_df],
                               ignore_index=True)
         return self.data
--- scraper/test.py
+++ scraper/test.py
@@ -1,62 +1,92 @@
-import requests
+import os
 from bs4 import BeautifulSoup
 import pandas as pd
+import requests
+from sqlalchemy import create_engine
 from datetime import datetime
-import urllib3
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
 
 class JobScraper:
     def __init__(self):
         self.data = None
 
-    def xalqbank(self):
-        print("Started scraping Xalqbank")
-        url = 'https://www.xalqbank.az/az/ferdi/bank/career'
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Accept-Encoding': 'gzip, deflate, br, zstd',
-            'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,ru;q=0.7,az;q=0.6',
-            'Cache-Control': 'max-age=0',
-            'Referer': 'https://www.google.com/',
-            'Dnt': '1',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-User': '?1',
-            'Upgrade-Insecure-Requests': '1',
-            'Cookie': '_gid=GA1.2.2120774294.1716196109; _ym_uid=1707748848536691364; _ym_d=1716196109; _ym_isad=2; _fbp=fb.1.1716196109680.1185575570; _ym_visorc=w; uslk_umm_1234_s=ewAiAHYAZQByAHMAaQBvAG4AIgA6ACIAMQAiACwAIgBkAGEAdABhACIAOgB7AH0AfQA=; _ga_Z2590XM715=GS1.1.1716196109.1.1.1716196497.60.0.0; _ga=GA1.1.544691763.1716196109'
-        }
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Ensure the request was successful
+    def offer_az(self):
+        start_page = 1
+        end_page = 10
+        try:
+            print("Started scraping offer.az")
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Encoding': 'gzip, deflate, br, zstd',
+                'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,ru;q=0.7,az;q=0.6',
+                'Cache-Control': 'max-age=0',
+                'Referer': 'https://www.google.com/',
+                'Dnt': '1',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-User': '?1',
+                'Upgrade-Insecure-Requests': '1'
+            }
 
-        soup = BeautifulSoup(response.content, 'html.parser')
+            vacancies = []
 
-        vacancies = []
-        vacancy_items = soup.find_all('a', class_='vacancies__item')
+            for page in range(start_page, end_page + 1):
+                url = f'https://www.offer.az/is-elanlari/page/{page}/'
+                response = requests.get(url, headers=headers)
+                response.raise_for_status()  # Ensure the request was successful
 
-        for item in vacancy_items:
-            category = item.find('span', class_='vacancies__category').text.strip()
-            title = item.find('h2', class_='vacancies__title').text.strip()
-            location = item.find('span', class_='vacancies__location').text.strip()
-            link = item['href']
-            vacancies.append([category, title, location, link])
+                soup = BeautifulSoup(response.content, 'html.parser')
 
-        df = pd.DataFrame(vacancies, columns=['company', 'vacancy', 'location', 'apply_link'])
-        df['company'] = 'xalqbank'
-        print("Scraping completed for Xalqbank")
-        return df
+                job_elements = soup.find_all('div', class_='job-card')
+
+                # Debugging: Check the number of job elements found
+                print(f"Found {len(job_elements)} job elements on page {page}")
+
+                for job_element in job_elements:
+                    title_tag = job_element.find('a', class_='job-card__title')
+                    company_tag = job_element.find('p', class_='job-card__meta')
+                    location_tag = job_element.find_all('p', class_='job-card__meta')[1]
+                    date_tag = job_element.find_all('p', class_='job-card__meta')[1]
+
+                    title = title_tag.text.strip() if title_tag else 'No title'
+                    company = company_tag.text.strip() if company_tag else 'No company'
+                    location = location_tag.text.strip().split(' - ')[1] if location_tag else 'No location'
+                    date = date_tag.text.strip().split(' - ')[0] if date_tag else 'No date'
+                    link = title_tag['href'] if title_tag and 'href' in title_tag.attrs else 'No link'
+
+                    vacancies.append([company, title, link])
+
+                    # Debugging: Print each job element
+                    print(f" Company: {company}, Vacancy: {title}, Link: {link}")
+
+            df = pd.DataFrame(vacancies, columns=['company','vacancy','apply_link'])
+            print("Scraping completed for offer.az")
+            return df
+        except Exception as e:
+            print(f"Offer.az scraping failed: {e}")
+            return pd.DataFrame(columns=['company', 'vacancy', 'apply_link'])
 
     def get_data(self):
-        xalqbank_df = self.xalqbank()
+        offer_az_df = self.offer_az()
 
         scrape_date = datetime.now()
 
-        xalqbank_df['scrape_date'] = scrape_date
+        offer_az_df['scrape_date'] = scrape_date
 
-        self.data = pd.concat([xalqbank_df],
+        self.data = pd.concat([ offer_az_df],
                               ignore_index=True)
         return self.data
 
+    def save_to_excel(self, filename):
+        if self.data is not None:
+            self.data.to_excel(filename, index=False)
+            print(f"Data saved to {filename}")
+        else:
+            print("No data to save.")
+
+# Example usage
+scraper = JobScraper()
+data = scraper.get_data()
+print(data)
+scraper.save_to_excel("job_vacancies.xlsx")
