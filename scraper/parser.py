b9fd1467c0d29b806fad88f28ca6db2bcc048dc4
Ismat <ismetsemedov@gmail.com>
2024-06-15 18:53:25 +0400

    konsis scraper added

--- scraper/parser.py
+++ scraper/parser.py
@@ -3147,6 +3147,54 @@ class JobScraper:
             else:
                 logger.warning("Vacancies section not found on the AzerGold page.")
         return pd.DataFrame(columns=['company', 'vacancy', 'apply_link'])
+    def parse_konsis(self):
+        logger.info("Started scraping Konsis")
+        url = "https://konsis.az/karyera-vakansiya/"
+        response = self.fetch_url(url, verify=False)  # Added verify=False to handle SSL issues
+        if response:
+            soup = BeautifulSoup(response.text, "html.parser")
+            logger.info("Page fetched successfully")
+
+            # Locate the articles containing the job listings
+            articles = soup.find_all("div", class_="grid-item")
+            if articles:
+                logger.info("Vacancies section found")
+                job_titles = []
+                job_companies = []
+                job_locations = []
+                job_types = []
+                job_descriptions = []
+                job_links = []
+
+                for article in articles:
+                    meta = article.find("div", class_="item--meta")
+                    if meta:
+                        job_title = meta.find("h3", class_="item--title").text.strip()
+                        features = meta.find_all("li")
+                        job_company = features[0].text.strip()
+                        job_location = features[1].text.strip()
+                        job_type = features[2].text.strip()
+                        job_description = article.find("div", class_="item-desc").text.strip()
+                        job_link = article.find("a", class_="btn btn-secondary", href=True)["href"]
+
+                        job_titles.append(job_title)
+                        job_companies.append(job_company)
+                        job_locations.append(job_location)
+                        job_types.append(job_type)
+                        job_descriptions.append(job_description)
+                        job_links.append("https://konsis.az" + job_link)
+
+                df = pd.DataFrame({
+                    'company': job_companies,
+                    'vacancy': job_titles,
+                    'apply_link': job_links
+                })
+                logger.info("Scraping completed for Konsis")
+                return df
+            else:
+                logger.warning("Vacancies section not found on the Konsis page.")
+        return pd.DataFrame(columns=['company', 'vacancy', 'apply_link'])
+
 
     def get_data(self):
         methods = [
@@ -3238,6 +3286,7 @@ class JobScraper:
             self.parse_tabib_vacancies,
             self.parse_projobs_vacancies,
             self.parse_azergold,
+            self.parse_konsis,
         ]
 
         results = []
