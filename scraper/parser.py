1d1611dd7152b79adfbdb832805359b3cb225498
Ismat <ismetsemedov@gmail.com>
2024-06-15 14:28:06 +0400

    added new scraper for projobs.az

--- scraper/parser.py
+++ scraper/parser.py
@@ -3043,6 +3043,7 @@ class JobScraper:
         return df
 
     def parse_tabib_vacancies(self):
+        print('Started scraping TABIB vacancies')
         logger.info("Started scraping TABIB vacancies")
         url = "https://tabib.gov.az/_next/data/VVczvdjPBoUR2khOC7cCO/az/vetendashlar-ucun/vakansiyalar.json"
         response = self.fetch_url(url)
@@ -3065,6 +3066,56 @@ class JobScraper:
             else:
                 logger.warning("No vacancies found in the API response.")
         return pd.DataFrame(columns=["company", "vacancy", "apply_link"])
+
+ 
+    def parse_projobs_vacancies(self):
+        """Fetch and parse job vacancies from Projobs API."""
+        data = []
+        base_url = "https://core.projobs.az/v1/vacancies"
+        headers = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,ru;q=0.7,az;q=0.6',
+            'Connection': 'keep-alive',
+            'Dnt': '1',
+            'Host': 'core.projobs.az',
+            'Origin': 'https://projobs.az',
+            'Referer': 'https://projobs.az/',
+            'Sec-Ch-Ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': '"macOS"',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+        }
+        max_pages=10
+        for page in range(1, max_pages + 1):
+            url = f"{base_url}?page={page}"
+            try:
+                response = requests.get(url, headers=headers)
+                response.raise_for_status()
+                vacancies = response.json().get("data", [])
+                for vacancy in vacancies:
+                    vacancy_info = {
+                        "company": vacancy["companyName"],
+                        "vacancy": vacancy["name"],
+                        "apply_link": f"https://projobs.az/jobdetails/{vacancy['id']}"
+                    }
+                    data.append(vacancy_info)
+                logger.info(f"Scraped page {page} successfully.")
+            except requests.RequestException as e:
+                logger.error(f"Request to {url} failed: {e}")
+                continue
+
+        if data:
+            df = pd.DataFrame(data)
+            logger.info("Scraping completed for Projobs")
+            return df
+        else:
+            logger.warning("No vacancies found in the API response.")
+            return pd.DataFrame(columns=["company", "vacancy", "apply_link"])
+
 
     def get_data(self):
         methods = [
@@ -3154,6 +3205,7 @@ class JobScraper:
             self.parse_djinni_co,
             self.parse_talhunt_az,
             self.parse_tabib_vacancies,
+            self.parse_projobs_vacancies,
         ]
 
         results = []
