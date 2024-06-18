04bc7a74c51d3422552eedbae10533f6aca238ef
Ismat <ismetsemedov@gmail.com>
2024-06-18 12:05:43 +0400

    new scraper added for jobsearch az

--- scraper/parser.py
+++ scraper/parser.py
@@ -12,6 +12,14 @@ import logging
 from sqlalchemy import create_engine, types
 from sqlalchemy.exc import SQLAlchemyError
 import re
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -3355,6 +3363,65 @@ class JobScraper:
             logger.error("Failed to fetch the page.")
             return None
 
+    def parse_classic_jobsearch_az(self):
+        logger.info("Started scraping classic.jobsearch.az")
+        
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # Ensure GUI is off
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        service = ChromeService(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver.get("https://classic.jobsearch.az/vacancies?date=&salary=&ads=&job_type=&location=&seniority=&ordering=&q=")
+
+        def scroll_page(driver, max_scrolls):
+            scroll_pause_time = 2  # Adjust to allow time for loading
+            scrolls = 0
+            last_height = driver.execute_script("return document.body.scrollHeight")
+            
+            while scrolls < max_scrolls:
+                driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
+                time.sleep(scroll_pause_time)
+                
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    break  # Exit loop if no more content is loaded
+                last_height = new_height
+                scrolls += 1
+
+        # Scroll 30 times to ensure more jobs are loaded
+        scroll_page(driver, 30)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'vacancies__item')))
+
+        jobs = []
+        elements = driver.find_elements(By.CLASS_NAME, 'vacancies__item')
+        seen_titles = set()
+        for index, element in enumerate(elements):
+            try:
+                title_element = element.find_element(By.CSS_SELECTOR, 'h2.vacancies__title > a')
+                company_element = element.find_element(By.CSS_SELECTOR, 'a.vacancies__provided > span')
+                link = title_element.get_attribute('href')
+                title = title_element.text.strip()
+                
+                if title not in seen_titles:
+                    job = {
+                        'company': company_element.text.strip(),
+                        'vacancy': title,
+                        'apply_link': link
+                    }
+                    jobs.append(job)
+                    seen_titles.add(title)
+            except Exception as e:
+                logger.error(f"Error processing element {index}: {e}")
+                continue
+
+        driver.quit()
+
+        df = pd.DataFrame(jobs)
+        logger.info("Scraping completed for classic.jobsearch.az")
+        return df
 
     def get_data(self):
         methods = [
@@ -3450,6 +3517,7 @@ class JobScraper:
             self.parse_baku_electronics,
             self.parse_asco,
             self.parse_cbar,
+            self.parse_classic_jobsearch_az,
         ]
 
         results = []
