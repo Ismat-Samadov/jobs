5088c3765689c6a2f1779348ecfc67d853898117
Ismat <ismetsemedov@gmail.com>
2024-08-10 00:08:09 +0400

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,8 +1,7 @@
-# api/api.py
 from fastapi import FastAPI, Depends, Query
 from database import connect_to_postgres, close_connection
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import Query
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
@@ -32,18 +31,31 @@ async def read_root():
 
 @app.get("/data/company/{company}")
 async def get_data_by_company(company: str, db=Depends(connect_to_postgres)):
-    query = "SELECT distinct * FROM public.jobs_jobpost WHERE  posted_at = (select max(posted_at) from public.jobs_jobpost ) and  company ILIKE $1 ORDER BY scrape_date DESC;"
+    query = """
+    SELECT distinct * 
+    FROM public.jobs_jobpost 
+    WHERE is_scraped = TRUE 
+    AND company ILIKE $1 
+    AND posted_at >= NOW() - INTERVAL '30 days'
+    ORDER BY posted_at DESC;
+    """
     rows = await db.fetch(query, f"%{company}%")
     await close_connection(db)
     return rows
 
 @app.get("/data/position/")
 async def get_data_by_position(position: str = Query(..., description="Position name to search for"), db=Depends(connect_to_postgres)):
-    query = "SELECT distinct * FROM public.jobs_jobpost WHERE posted_at = (select max(posted_at) from public.jobs_jobpost ) and vacancy ILIKE $1 ORDER BY scrape_date DESC;"
+    query = """
+    SELECT distinct * 
+    FROM public.jobs_jobpost 
+    WHERE is_scraped = TRUE 
+    AND title ILIKE $1 
+    AND posted_at >= NOW() - INTERVAL '30 days'
+    ORDER BY posted_at DESC;
+    """
     rows = await db.fetch(query, f"%{position}%")
     await close_connection(db)
     return rows
-
 
 @app.get("/data/")
 async def get_data(
@@ -53,26 +65,31 @@ async def get_data(
     page_size: int = Query(10, description="Number of items per page"),
     db=Depends(connect_to_postgres)
 ):
-    if company is None and position is None:
-        # If neither company nor position is provided, return all data
-        query = "SELECT distinct * FROM public.jobs_jobpost where posted_at = (select max(posted_at) from public.jobs_jobpost ) ORDER BY posted_at DESC OFFSET $1 LIMIT $2;"
-        offset = (page - 1) * page_size
-        rows = await db.fetch(query, offset, page_size)
-    elif company is not None and position is not None:
-        # If both company and position are provided, search by both
-        query = "SELECT distinct * FROM public.jobs_jobpost WHERE posted_at = (select max(posted_at) from public.jobs_jobpost ) and company ILIKE $1 AND vacancy ILIKE $2 ORDER BY posted_at DESC OFFSET $3 LIMIT $4;"
-        offset = (page - 1) * page_size
-        rows = await db.fetch(query, f"%{company}%", f"%{position}%", offset, page_size)
-    elif company is not None:
-        # If only company is provided, search by company with LIKE
-        query = "SELECT distinct * FROM public.jobs_jobpost WHERE posted_at = (select max(posted_at) from public.jobs_jobpost ) and company ILIKE $1 ORDER BY posted_at DESC OFFSET $2 LIMIT $3;"
-        offset = (page - 1) * page_size
-        rows = await db.fetch(query, f"%{company}%", offset, page_size)
-    else:
-        # If only position is provided, search by position
-        query = "SELECT distinct * FROM public.jobs_jobpost WHERE posted_at = (select max(posted_at) from public.jobs_jobpost ) and vacancy ILIKE $1 ORDER BY posted_at DESC OFFSET $2 LIMIT $3;"
-        offset = (page - 1) * page_size
-        rows = await db.fetch(query, f"%{position}%", offset, page_size)
+    base_query = """
+    SELECT distinct * 
+    FROM public.jobs_jobpost 
+    WHERE is_scraped = TRUE 
+    AND posted_at >= NOW() - INTERVAL '30 days'
+    """
+
+    filters = []
+    parameters = []
+
+    if company:
+        filters.append("company ILIKE $1")
+        parameters.append(f"%{company}%")
+    if position:
+        filters.append("title ILIKE $2")
+        parameters.append(f"%{position}%")
+    
+    if filters:
+        base_query += " AND " + " AND ".join(filters)
+    
+    base_query += " ORDER BY posted_at DESC OFFSET $3 LIMIT $4;"
+
+    offset = (page - 1) * page_size
+    parameters.extend([offset, page_size])
+
+    rows = await db.fetch(base_query, *parameters)
     await close_connection(db)
     return rows
-
