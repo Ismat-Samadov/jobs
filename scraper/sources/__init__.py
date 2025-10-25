fd14dee74f2cf7cb632f9dbb08de425e20248350
ismat-samadov <ismetsemedov@gmail.com>
2025-10-25 20:25:14 +0400

    #8

--- scraper/sources/__init__.py
+++ scraper/sources/__init__.py
@@ -3,9 +3,11 @@ from .evv_az_scraper import EvvAzScraperAsync
 from .villa_az_scraper import VillaAzScraperAsync
 from .bul_az_scraper import BulAzScraperAsync
 from .turbo_az_scraper import TurboAzScraper, scrape_turbo_az
+from .avtopro_az_scraper import AvtoproAzScraperAsync
 
-__all__ = ['EvvAzScraperAsync', 
-           'VillaAzScraperAsync', 
-           'BulAzScraperAsync', 
-           'TurboAzScraper', 
-           'scrape_turbo_az']
+__all__ = ['EvvAzScraperAsync',
+           'VillaAzScraperAsync',
+           'BulAzScraperAsync',
+           'TurboAzScraper',
+           'scrape_turbo_az',
+           'AvtoproAzScraperAsync']
