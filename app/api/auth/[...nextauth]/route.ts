65e2b002705b1bda7321a7d2ebdf1c604da9da82
Ismat-Samadov <ismetsemedov@gmail.com>
2026-03-14 23:33:27 +0400

    hard reset

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-/**
- * NextAuth API route handler
- */
-import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/nextauth';
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
